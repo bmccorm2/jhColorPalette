ebcd3822233972dc76a19a28b2b36d0bac8e520b
bryan <bryan@1mccormick.com>
2023-01-10 16:05:29 -0700

    bug

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,16 @@
 <div class="container mx-auto">
-	<div class="flex justify-between items-center mt-4 mb-4">
+	<div class="flex justify-between items-center mt-4 mb-8">
 		<img src="/handCoin.png" class="h-12" alt="coin" />
-		<img src="/logoS.png" class="w-1/3" alt="logo" />
+		<img src="/logoW.png" class="w-1/4" alt="logo" />
 	</div>
 	<div class="header bg-jhTangerine-100 mt-2 text-jhSlate-100 font-header">
 		Slate
 	</div>
-	<div class="header bg-jhSlate-100 text-white mt-2 font-body">Slate</div>
+	<div
+		class="header border border-white bg-jhSlate-100 text-white mt-2 font-body"
+	>
+		Slate
+	</div>
 	<div class="header bg-jhCerulean-100 text-white mt-2 font-sat">Cerulean</div>
 	<div class="header bg-jhMerlot-100 text-white mt-2">Merlot</div>
 	<div class="header bg-jhRoyal-100 text-white mt-2">Royal</div>
